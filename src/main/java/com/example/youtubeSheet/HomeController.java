474ac0843ba45dab465d13684d2bcc0cfcdffcbb
GyeongHwa <gyeonghwaa_@naver.com>
2024-07-18 19:35:45 +0900

    Add /serverCheck api

--- src/main/java/com/example/youtubeSheet/HomeController.java
+++ src/main/java/com/example/youtubeSheet/HomeController.java
@@ -3,6 +3,8 @@ package com.example.youtubeSheet;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 @Slf4j
 @Controller
@@ -20,5 +25,25 @@ public class HomeController {
 
         return "redirect:/post/list";    }
 
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+
+    @Value("${serverName}")
+    private String serverName;
+
+    @GetMapping("/serverCheck")
+    public ResponseEntity<?> serverCheck(){
+        Map<String,String> responseData=new TreeMap<>();
+
+        responseData.put("Server address",serverAddress);
+        responseData.put("Server name", serverName);
+        responseData.put("Port number",port);
+
+        return ResponseEntity.ok(responseData);
+    }
 
 }
